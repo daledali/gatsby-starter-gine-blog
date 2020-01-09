2131429e8b70638549de149a49d230dfae158485
djtlgucla <51318912+djtlgucla@users.noreply.github.com>
2020-01-09 02:11:55 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/b4af861710064848aaf9f859c79bb127?v=0de244d6f8414ade9b7b147adcb2f78e"
+                configTable: "https://www.notion.so/dtlmd/219d25842fb840b8bcde831ac76f43c9?v=ea2cc12823034a478592986ddf8efe9b"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/65b35da1762e4f259904bb4cc38e54fd?v=89e80623e52e4419a35f20e54056cfb8"
+                configTable: "https://www.notion.so/dtlmd/aa0b9f9c724c483b85662f183c8c2c46?v=ffc44bf4e2ca4b6caa1b0f57bede9762"
             }
         }
     ],
